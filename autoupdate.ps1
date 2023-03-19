26ea78d3dff20988ab0aeaf22e4e6dfa8170efa2
Dawid Goslawski <dawid@dawidgoslawski.pl>
2023-03-19 17:51:43 +0100

    Fix workflow

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -4,5 +4,5 @@ $out = (& $script -Dir $PWD/bucket -Update 6>&1)
 Write-Host $out
 
 if (Write-Output $out | Select-String "Autoupdating") {
-    Write-Output "::set-output name=updated::true"
+    Write-Output "updated=true" >> $env:GITHUB_ENV
 }
